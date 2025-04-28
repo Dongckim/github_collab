54af7081dacae4cb3c68807dcc9de80aae374d91
김범준 <beomjunkim@Beomjuns-MacBook-Pro-14.local>
2025-04-28 19:38:34 +0900

    validate python file re-commit

--- src/validate.py
+++ src/validate.py
@@ -1,5 +1,15 @@
+# import json
+# import os
+# current_dir = os.path.dirname(__file__)
+# json_file_path = os.path.join(current_dir, '..', 'data', 'newLambdaComers.json')
+#
+# # Normalize the path
+# json_file_path = os.path.abspath(json_file_path)
+#
+# # Load the JSON
+# with open(json_file_path, 'r', encoding='utf-8') as f:
+#     students = json.load(f)
 def validate_json(students_data):
-
     for member in students_data:
         name = member.get('name')
         scores = member.get('scores')
@@ -14,3 +24,5 @@ def validate_json(students_data):
                 break
         if not scores:
             print(f"Scores for {name} is not available")
+
+# validate_json(students)
