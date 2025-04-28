602f664cccd4509dbe77fab95e9bf3cce32051ef
minseokim <minseohappy07@gmail.com>
2025-04-28 13:56:24 +0900

    output to csv function added

--- src/output.py
+++ src/output.py
@@ -1,10 +1,10 @@
 import csv
 
 def save_to_csv(students_data):
-    with open('output.csv', 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(['name', 'average'])
-        for student in students_data:
-            writer.writerow([student['name'], student['average']])
-        
+    f = open('output.csv', 'w', newline='') 
+    writer = csv.writer(f)
+    writer.writerow(['name', 'average'])
+    for student in students_data:
+        writer.writerow([student['name'], student['average']])
+    f.close()
 
